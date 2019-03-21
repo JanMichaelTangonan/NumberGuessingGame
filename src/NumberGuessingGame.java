201759ab24e07f3722c974a16ea3055f1c4f0e5c
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-03-21 14:19:34 -0400

    Trying to make game loop properly

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Give me a random number from 1 to 100.");
 
@@ -11,24 +11,45 @@ public class NumberGuessingGame {
 
         int pickedNumber;
 
-        pickedNumber = (int)(Math.random()*100);
+        pickedNumber = (int) (Math.random() * 100);
 
         System.out.println(pickedNumber);
 
-        if (pickedNumber>number){
+        boolean repeatGame= true;
 
-            System.out.println("Your chosen number is lower than the random number that we have chosen.");
+        while (repeatGame){
+
+            //Results indicators (detects if the game needs repeating or not)
+            if (pickedNumber > number) {
+
+                System.out.println("Your chosen number is lower than the random number that we have chosen.");
+
+            } else if (pickedNumber < number) {
+
+                System.out.println(("Your chosen number is higher than the random number that we have chosen."));
+
+            }
+
+            if (pickedNumber == number) {
+
+                System.out.println("You must have eaten lucky charms or something...");
+                break;
+            }
+
         }
-        else if(pickedNumber<number){
 
-            System.out.println(("Your chosen number is higher than the random number that we have chosen."));
+        //Counting how many tries are allowed (DO NOT WORK ON JUST YET)
+        for(int tries = 0;tries<30;){
+
+        System.out.println("You have guessed too much, guess your just unlucky :(");
         }
-        if (pickedNumber==number){
 
-            System.out.println("You have gotten the random number, congratulations :D!");
 
-        }
+
+
+
     }
-
 }
+
+
 
